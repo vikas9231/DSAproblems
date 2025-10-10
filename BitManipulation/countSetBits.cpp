2b422a509022e472912c6cb8363b023a232d92ab
vikas9231 <smumv2009@gmail.com>
2025-10-10 14:40:53 +0530

    count of set bits

---
+++ BitManipulation/countSetBits.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countSetBit(int n){
+    int res =0;
+    while(n>0){
+        if(n%2 == 1) res++;
+        n = n/2;
+    }
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << countSetBit(n);
+    return 0;
+}
